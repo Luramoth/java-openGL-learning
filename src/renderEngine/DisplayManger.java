1fae44e66fb9d20c84ed84be78dada84bea8ccb5
Luramoth <fasreaperearnest@gmail.com>
2023-01-07 19:59:47 -0800

    initialise display

---
+++ src/renderEngine/DisplayManger.java
@@ -0,0 +1,30 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+
+public class DisplayManger {
+
+	private static final int WIDTH = 1280;
+	private static final int HEIGHT = 720;
+
+	public static void createDisplay(){
+
+		ContextAttribs attribs =  new ContextAttribs(3,2);
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat());
+		} catch (LWJGLException e) {
+			throw new RuntimeException(e);
+		}
+
+		GL11.glViewport(0,0,WIDTH,HEIGHT);
+	}
+
+	public static void updateDisplay(){}
+
+	public static void closeDisplay(){}
+}
